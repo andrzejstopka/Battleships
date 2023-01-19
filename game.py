108555aee569bbe9a53567dbe979d3c4d1206c87
andrzej_stopka <andrzej.stopka1996@gmail.com>
2023-01-19 22:17:18 +0100

    Check_ship_surrounding to check all pieces of ship and implement it to place_user_ship

--- game.py
+++ game.py
@@ -44,33 +44,38 @@ class Board():
                 print("Invalid input")
                 continue
             x, y, h = self.convert_user_input(user_input)   # h - horizon
+            places = []  # checked places, ship is not added when is checked because it would corrupt check_ship_surrounding function
             if h == "h":
                 for space in range(ship.occupied_spaces()):
-                    self.board[x][y + space] = "0"
+                    if self.check_ship_surrounding(x, y + space) is False: # when any piece of ship is wrong, ship is not addded.
+                        print("Invalid space")
+                        break
+                    places.append((x, y + space)) 
             elif h == "v":
                 for space in range(ship.occupied_spaces()):
-                    self.board[x + space][y] = "0"
+                    if self.check_ship_surrounding(x + space, y) is False: # when any piece of ship is wrong, ship is not addded.
+                        print("Invalid space")
+                        break
+                    places.append((x + space, y))
+            if len(places) == ship.occupied_spaces():
+                for place in places:
+                        self.board[place[0]][place[1]] = "0"   # if all spaces is correct, ship is added to the board
+
+                
  
-    def ship_surrounding(self, ship, board, column, row, direction):
-        for space in range(3):
-            if direction == 'h':
-                if column != 0 and board[column-1][row+space-1] == '0':
-                    return False
-                if column != 9 and board[column+ship.occupied_spaces()][row+space-1] == '0':
-                    return False
-                if row != 0 and board[column+space-1][row-1] == '0':
-                    return False
-                if row != 9 and board[column+space-1][row+1] == '0':
-                    return False
-            elif direction == 'v':
-                if row != 0 and board[column+space-1][row-1] == '0':
-                    return False
-                if row != 9 and board[column+space-1][row+ship.occupied_spaces()] == '0':
-                    return False
-                if column != 0 and board[column-1][row+space-1] == '0':
-                    return False
-                if column != 9 and board[column+1][row+space-1] == '0':
-                    return False
+    def check_ship_surrounding(self, x, y):
+        # check if any surrounding spaces are occupied
+        try:
+            if self.board[x - 1][y] != "~":
+                return False
+            elif self.board[x + 1][y] != "~":
+                return False
+            elif self.board[x][y - 1] != "~":
+                return False
+            elif self.board[x][y + 1] != "~":
+                return False
+        except IndexError:  # if x would be "J" or y would be 10, it mean that add ship is possible 
+            pass
         return True
  
 class Ship:
